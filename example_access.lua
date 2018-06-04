992d21ea92e82c7758e5dd064b507eaf3ad3f655
南琴浪 <contacts@sometimesnaive.org>
2018-06-05 03:02:58 +0800

    fix variable

--- example_access.lua
+++ example_access.lua
@@ -1,3 +1,5 @@
+local iputils = require "resty.lua_resty_minami_iputils.iputils"
+
 --variable 'list' is defined at 'init' phrase
 local ok, err = iputils.iputils_binipincidrs(ngx.var.binary_remote_addr, list)
 
