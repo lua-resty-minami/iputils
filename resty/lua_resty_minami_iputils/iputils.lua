02f27cffa81f04890db4c9b17d84aecb5e00ceaf
南琴浪 <26526543+nanqinlang@users.noreply.github.com>
2018-06-13 01:10:53 +0800

    localize

--- resty/lua_resty_minami_iputils/iputils.lua
+++ resty/lua_resty_minami_iputils/iputils.lua
@@ -1,9 +1,11 @@
 local _M = {}
 
 
+local iputils = require "resty.lua_resty_iputils.iputils"
+
 function _M.table(filedir)
 	--initialize table
-	ips_table = {}
+	local ips_table = {}
 
 	--read contents lines by lines
 	local filename = io.lines(filedir)
@@ -17,19 +19,22 @@ end
 
 function _M.iputils_lrucache(zone)
 	--create a global lrucache object
-	return require("resty.lua_resty_iputils.iputils").lrucache_enable(zone)
+	local  lrucache_enable = iputils.lrucache_enable(zone)
+	return lrucache_enable
 end
 
 function _M.iputils_parsecidrs(table)
 	--takes a table of CIDR format IPV4 networks and returns a table containing the lower and upper addresses
 	--if an invalid network is in the table, an error will be logged and the other networks will be returned
-	return require("resty.lua_resty_iputils.iputils").parse_cidrs(table)
+	local  parse_cidrs = iputils.parse_cidrs(table)
+	return parse_cidrs
 end
 
 function _M.iputils_binipincidrs(ip, table)
 	--returns a true or false if the IP exists within any of the specified networks
 	--returns nil and an error message with an invalid IP
-	return require("resty.lua_resty_iputils.iputils").binip_in_cidrs(ip, table)
+	local  binip_in_cidrs = iputils.binip_in_cidrs(ip, table)
+	return binip_in_cidrs
 end
 
 
